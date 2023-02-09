f00a542c0d20e0ab823f1e37b413b7eb5a7384be
Yusuf Sargin <ysargin@atlassian.com>
2023-02-09 19:53:50 +0300

    secure

--- src/main/kotlin/com/learning/kotlinlearning/config/SecurityConfig.kt
+++ src/main/kotlin/com/learning/kotlinlearning/config/SecurityConfig.kt
@@ -1,4 +1,17 @@
 package com.learning.kotlinlearning.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
 class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+
+    }
 }
